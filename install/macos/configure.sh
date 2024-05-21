bc582aaeca6a4adc6217cd465b52981dfc595385
Joseph Lee <joel@joeworks.com>
2024-05-21 22:44:08 +0100

    add MacOS Configuration script

---
+++ install/macos/configure.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Work in progress: Not stable yet"
+exit 0
+
+BASE_PATH="$(dirname $0)"
+source "$BASE_PATH/../../init.sh"
+
+BREW_FILE="${BASE_PATH}/Brewfile"
+
+if [ -z "$(which brew)" ]; then
+    echo "Homebrew was not installed."
+    exit 1
+fi
+
+brew update && brew upgrade
+brew bundle --file="${BREW_FILE}"
+
+# Configure packages
+dot_configure git fish alacritty kitty starship tmux
