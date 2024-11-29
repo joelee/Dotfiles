b60840fc65c47e5be205a78c1bce3fac7304d568
Joseph Lee <joel@joeworks.com>
2024-11-29 15:10:30 +0000

    Improve CachyOS set up and add Yubikeys

--- install/cachyos/install_packages.sh
+++ install/cachyos/install_packages.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 #
 
-source "$DOTFILES_HOME/common/common_functions.bash"
+BASE_PATH="$(dirname $0)"
+source "${BASE_PATH}/../../common/common_functions.bash"
 
 PACMAN="$(which pacman)"
 if [ -z "${PACMAN}" ]; then
 	echo "pacman not found. This script only works on Arch Linux based distribution"
+	exit 1
+fi
+
+if [ -z "$(which cachyos-hello)" ]; then
+	echo "CachyOS is not detected. This install script is meant for CachyOS distribution only."
 	exit 1
 fi
 
@@ -41,8 +47,7 @@ sudo pacman -S --noconfirm \
 	nodejs \
 	npm \
 	nvm \
-	ollama \
-    docker \
+	docker \
 	docker-compose \
 	pre-commit \
 	pyenv \
@@ -86,20 +91,32 @@ sudo pacman -S --noconfirm \
 	vscodium
 
 # Required to run certain AppImages (eg. Bambu Studio)
-sudo pacman -S fuse2 webkit2gtk webkit2gtk-4.1
+# sudo pacman -S fuse2 webkit2gtk webkit2gtk-4.1
 
 
 # Required to enable Yubikey authentication
 sudo pacman -S pam-u2f yubikey-manager yubico-pam
 
 
+# Hardware specific installation
+
+# For CUDA supported system
+# sudo pacman -S noconfirm ollama-cuda
+
+# For non-CUDA supported system
+# sudo pacman -S noconfirm ollama
+
+
 # App packages I use: installed by yay
 #
 # yay -S --noconfirm novelwriter
 # yay -S --noconfirm orca-slicer
+# yay -S --noconfirm bambustudio-bin
 # yay -S --noconfirm pycharm-professional
 # yay -S --noconfirm logseq-desktop
 # yay -S --noconfirm obsidian
+# yay -S --noconfirm ticktick
+
 
 # Enable services
 sudo systemctl enable docker.service
@@ -109,4 +126,6 @@ sudo systemctl enable containerd.service
 touch "${DOTFILES_LOCAL_PATH}/packages_installed"
 echo ""
 echo "Package installation completed."
+echo ""
+
 
