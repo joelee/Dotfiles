aa0e5d6e39541db5498abf6f35718a31fe29f2ca
Joseph Lee <joseph.lee@bjss.com>
2024-12-09 10:40:56 +0000

    Update for MacOS. Removing ZSH packages in favour of FISH

---
+++ install/macos/install_packages.sh
@@ -0,0 +1,15 @@
+brew install \
+	aws-cdk aws-sam-cli awscli azure-cli bat black btop certbot cfn-lint \
+	curl libtool direnv duckdb eza fastfetch fd ffmpeg \
+	fio fish fzf git glances gnupg go golines graphviz helix htop ifstat \
+	imagemagick imagesnap jq kind lazygit lolcat neovim neovide ninja nnn nvm \
+	pipx pkg-config poetry prettier pyenv pyenv-virtualenv pyright python-lsp-server \
+	pytorch rbenv rclone rdiff-backup ripgrep rust-analyzer rustup-init sevenzip \
+	showkey starship switchaudio-osx terminal-notifier tldr tmux wakeonlan watch \
+	wget zellij zoxide 
+
+brew install \
+	alacritty amethyst dbeaver-community drawio easy-move-plus-resize gimp google-cloud-sdk \
+	grammarly-desktop hightop kdiff3 libreoffice postman sourcetree
+
+
